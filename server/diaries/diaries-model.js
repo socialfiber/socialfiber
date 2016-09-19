6bdfb1ec4cf3908ff38750590aa62e0c0a69b079
Sam Kim <samkim28@gmail.com>
2016-09-19 10:49:00 -0700

    [setup] adds diaries model

--- server/diaries/diaries-model.js
+++ server/diaries/diaries-model.js
@@ -1,2 +1,24 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
+const diaryEntries = require('../diaryEntries-model');
+
+const Diaries = sequelize.define('diaries', {
+  user_id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    allowNull: false
+  }
+});
+
+//one to many relationship
+Diaries.hasMany(DiaryEntries);
+
+sequelize
+  .sync()
+  .then(function(err) {
+    console.log('Diaries model synced successfully.');
+}, function(err) {
+    console.log('An error has occurred:', err);
+  });
+
+module.exports = Diaries;
