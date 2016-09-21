74b84677722d49c64e79df6d313fe180ce601169
Calvin Ton <cton43@yahoo.com>
2016-09-20 20:33:16 -0700

    [feature] adds compare password hash

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const moment = require('moment');
 
 const app = express();
 const database = require('./config/database.js');
@@ -23,6 +24,5 @@ app.set('port', process.env.PORT || 8080);
 
 app.listen(app.get('port'), () => {
   //database.ensureSchema();
-  //moment().format('h:mm:ss a')
-  console.log('Server is Listening on port', app.get('port'));
+  console.log('[',moment().format('h:mm:ss a'),'] Server is Listening on port', app.get('port'));
 });
