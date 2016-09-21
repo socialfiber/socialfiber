f4ce28db0003f92fb77bd7578dd738e8d05caae9
Calvin Ton <cton43@yahoo.com>
2016-09-21 12:22:49 -0700

    [refactor] cleans up API ctrl

--- server/nutritionix/nutritionix-ctrl.js
+++ server/nutritionix/nutritionix-ctrl.js
@@ -2,10 +2,6 @@ const Nutritionix = require('./nutritionix-model.js');
 
 const nutritionix = {
 	'/api/nutritionix/search': {
-		'get': (req, res) => {
-			console.log('inside GET at /api/nutritionix/search');
-			res.end('inside GET at /api/nutritionix/search');
-		},
 		'post': (req, res) => {
 			Nutritionix.search(req.body)
 			.then((data) => {
@@ -13,14 +9,6 @@ const nutritionix = {
 			}).catch((err) => {
 				res.end('err inside nutritionix-ctrl.js', err);
 			})
-		},
-		'put': (req, res) => {
-			console.log('inside PUT at /api/nutritionix/search');
-			res.end('inside PUT at /api/nutritionix/search');
-		},
-		'delete': (req, res) => {
-			console.log('inside DELETE at /api/nutritionix/search');
-			res.end('inside DELETE at /api/nutritionix/search');
 		}
 	}
 }
