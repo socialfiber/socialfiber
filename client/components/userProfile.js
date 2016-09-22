822d46ed640ee2e00ae9c5a8c675fc14c9885121
Christian Borja <cpborja32@gmail.com>
2016-09-22 15:36:31 -0700

    [pull] progression on user profile feature

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -6,13 +6,27 @@ class UserProfile extends Component {
   componentWillMount() {
     this.props.fetchUserData();
   }
+  // renderUserData() {
+  //   console.log("This is this.props: ", this.props.userData)
+  //   return this.props.userData.map((data) => {
+  //     return <div>{data}</div>;
+  //   })
+  // }
   render() {
     return (
       <div>
         <h3>User Info</h3>
+        {/* {this.renderUserData()} */}
       </div>
     );
   }
 }
 
+// function mapStateToProps(state) {
+//   return {
+//     userData: state.UserProfile
+//   }
+// }
+
+>>>>>>> [pull] progression on user profile feature
 export default connect(null, { fetchUserData })(UserProfile);
