7b0b84243c8518ff635294cca13e7c06ea69f970
Christian Borja <cpborja32@gmail.com>
2016-09-22 16:58:20 -0700

    [feature] user data renders on user profile page

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -4,28 +4,41 @@ import { fetchUserData } from '../actions/fetchUserData';
 
 class UserProfile extends Component {
   componentWillMount() {
+    console.log('action');
     this.props.fetchUserData();
   }
   // renderUserData() {
-  //   console.log("This is this.props: ", this.props.userData)
-  //   return this.props.userData.map((data) => {
-  //     return <div>{data}</div>;
-  //   })
+  //   console.log("This is this.props.userData: ", this.props.userData)
+  //   // return this.props.userData.map((data) => {
+  //   //   return <div>{data}</div>;
+  //   // })
   // }
   render() {
-    return (
-      <div>
-        <h3>User Info</h3>
-        {/* {this.renderUserData()} */}
-      </div>
-    );
+    console.log("this.props: ", this.props.userData)
+    if(this.props.userData !== null) {
+      return (
+        <div>
+          <h3>User Info</h3>
+          <div>Age: {this.props.userData.age}</div>
+          <div>Gender: {this.props.userData.gender}</div>
+          <div>Height: {this.props.userData.height}</div>
+          <div>Weight: {this.props.userData.weight}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Loading your profile...</h3>
+        </div>
+      )
+    }
   }
 }
 
-// function mapStateToProps(state) {
-//   return {
-//     userData: state.UserProfile
-//   }
-// }
+function mapStateToProps(state) {
+  return {
+    userData: state.userProfile
+  }
+}
 
-export default connect(null, { fetchUserData })(UserProfile);
+export default connect(mapStateToProps, { fetchUserData })(UserProfile);
