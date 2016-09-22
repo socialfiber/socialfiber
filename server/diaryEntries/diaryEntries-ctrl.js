63c71949acdc1c634fbbc82d87d1f5e46585e2eb
Sam Kim <samkim28@gmail.com>
2016-09-22 16:57:52 -0700

    [setup] modifies diaryEntries model with more fields and adds dummy post and get requests in the controller

--- server/diaryEntries/diaryEntries-ctrl.js
+++ server/diaryEntries/diaryEntries-ctrl.js
@@ -1,1 +1,16 @@
 const DiaryEntries = require('./diaryEntries-model.js');
+
+const diaryEntries = {
+	'/api/diaryEntries/createEntry': {
+		'get': (req, res) => {
+			console.log('inside GET at /dummy_endpoint');
+			res.end('inside GET at /dummy_endpoint');
+		},
+		'post': (req, res) => {
+			console.log('inside POST at /dummy_endpoint');
+			res.end('inside POST at /dummy_endpoint');
+		}
+	}
+}
+
+module.exports = dummy;
