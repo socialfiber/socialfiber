099569d45459ac186946cfc11cfadcfba55fb7e6
Sam Kim <samkim28@gmail.com>
2016-09-22 19:18:16 -0700

    [setup] adds diary entries get method in the controller

--- server/diaryEntries/diaryEntries-ctrl.js
+++ server/diaryEntries/diaryEntries-ctrl.js
@@ -3,14 +3,33 @@ const DiaryEntries = require('./diaryEntries-model.js');
 const diaryEntries = {
 	'/api/diaryEntries/createEntry': {
 		'get': (req, res) => {
-			console.log('inside GET at /dummy_endpoint');
-			res.end('inside GET at /dummy_endpoint');
-		},
-		'post': (req, res) => {
-			console.log('inside POST at /dummy_endpoint');
-			res.end('inside POST at /dummy_endpoint');
+      var entryData = [];
+			console.log('inside GET at /api/diaryEntries/createEntry');
+      //find one , order by results
+      const getDiaryEntry = DiaryEntries.findAll({
+        limit: 1,
+        where: {
+          user_id: req.query.userID
+        }
+        order: [
+          ['date', 'DESC']
+        ]
+      })
+      .then( (entries) => {
+        entries.forEach( (entry) => {
+          console.log('entry : ', entry);
+          entryData.push(entry);
+        });
+        console.log('sending data');
+        console.log('entryData: ', entryData);
+        res.json(userData);
+      })
+      .catch( (err) => {
+        console.log('Error: ', err);
+        res.status(400).send(err.message);
+      });
 		}
-	}
+  }
 }
 
-module.exports = dummy;
+module.exports = diaryEntries;
