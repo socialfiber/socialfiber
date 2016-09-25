c198cb8e32d13bf82f7e4b8b9c4a4c0c14d4e8c8
Lauren Settle <laurjsett@gmail.com>
2016-09-24 17:17:10 -0700

    [pull] adds error message if confirmation password does not match

--- client/components/signup.js
+++ client/components/signup.js
@@ -4,25 +4,30 @@ import { Field, reduxForm } from 'redux-form';
 import { submitSignUp } from '../actions/auth';
 
 class SignUp extends Component {
-
   render() {
     const { handleSubmit } = this.props;
+    const wrongPass = (<div style={{color: 'red'}}>{this.props.auth}</div>);
     return (
-      <form onSubmit={handleSubmit(this.props.submitSignUp)}>
-        <div>
-          <label>Username</label>
-          <Field name="username" component="input" type="text" />
-        </div>
-        <div>
-          <label>Password</label>
-          <Field name="password" component="input" type="password" />
-        </div>
-        <div>
-          <label>Password</label>
-          <Field name="confirmPW" component="input" type="password" />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+      <div>
+      <pre><code>{JSON.stringify(this.props, null, 4)}</code></pre>
+        <form onSubmit={handleSubmit(this.props.submitSignUp)}>
+          <div>
+            <label>Username</label>
+            <Field name="username" component="input" type="text" />
+          </div>
+          <div>
+            <label>Password</label>
+            <Field name="password" component="input" type="password" />
+            {wrongPass}
+          </div>
+          <div>
+            <label>Password</label>
+            <Field name="confirmPW" component="input" type="password" />
+            {wrongPass}
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     )
   }
 
@@ -32,4 +37,8 @@ SignUp = reduxForm({
   form: 'SignUpForm'
 })(SignUp);
 
-export default connect(null, { submitSignUp })(SignUp);
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, { submitSignUp })(SignUp);
