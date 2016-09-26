02ca4821038fc9661a5a4236a40cb721bbde693a
Lauren Settle <laurjsett@gmail.com>
2016-09-26 15:14:35 -0700

    [fix] imports fetchAllGroups action

--- client/components/allGroups.js
+++ client/components/allGroups.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {  }
+import { fetchAllGroups } from '../actions/groups';
 import NavBar from './navbar';
 
 
