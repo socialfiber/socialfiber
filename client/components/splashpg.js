d4d15ac9de4968ae34c37ce4ce98a920577def18
Lauren Settle <laurjsett@gmail.com>
2016-09-26 12:36:15 -0700

    [pull] removes styling, uses img tag

--- client/components/splashpg.js
+++ client/components/splashpg.js
@@ -7,10 +7,10 @@ import NavBar from './navbar'
 export default class SplashPg extends Component {
   render(){
     return(
-    <div class = 'splashpg-div'>
+    <div className = 'splashpg-div'>
     <NavBar />
+    <h1>welcome</h1>
       <img src ='https://images.unsplash.com/photo-1473631706567-f3724ae047b5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=2b42cea7ba5c5603fed6b0e241c0cd14'></img>
-      <h1>welcome</h1>
     </div>
     )
   }
