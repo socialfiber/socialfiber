31afcd5c48b23253c1e620f84c525389b5e0cd7d
Lauren Settle <laurjsett@gmail.com>
2016-09-26 15:05:36 -0700

    [pull] completes fetch all groups action, creates groups reducer folder

--- client/actions/groups.js
+++ client/actions/groups.js
@@ -3,5 +3,14 @@ import { browserHistory } from 'react-router';
 import { FETCH_ALL_GROUPS } from './types';
 
 export function fetchAllGroups(){
-  return axios.get('')
+  return axios.get('/api/groups/getAllGroups')
+    .then(function(response){
+      return {
+        type: FETCH_ALL_GROUPS,
+        payload: response
+      };
+    })
+    .catch(function(error){
+      console.error(error);
+    })
 }
