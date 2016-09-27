f6ba4a9f42534f6159e923cddbe3dde95c849363
Lauren Settle <laurjsett@gmail.com>
2016-09-27 12:31:58 -0700

    [fix] fixes merge conflict

--- client/reducers/groups_reducer.js
+++ client/reducers/groups_reducer.js
@@ -1,10 +1,4 @@
-<<<<<<< 732003b2e026c2ab4c9d5d241a4fc7f0f0e64d22
 import { FETCH_ALL_GROUPS, FETCH_USER_GROUPS, CREATE_NEW_GROUP } from '../actions/types';
-
-=======
-import { FETCH_ALL_GROUPS } from '../actions/types';
->>>>>>> [fix] corrected import statement in index.js
-
 const INITIAL_STATE = {all:[]}
 
 export default function(state=INITIAL_STATE, action){
