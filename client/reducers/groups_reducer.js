732003b2e026c2ab4c9d5d241a4fc7f0f0e64d22
Lauren Settle <laurjsett@gmail.com>
2016-09-27 11:57:10 -0700

    [pull] adds route to creategroup page

--- client/reducers/groups_reducer.js
+++ client/reducers/groups_reducer.js
@@ -1,15 +1,16 @@
-import { FETCH_ALL_GROUPS, FETCH_USER_GROUPS } from '../actions/types';
+import { FETCH_ALL_GROUPS, FETCH_USER_GROUPS, CREATE_NEW_GROUP } from '../actions/types';
+
 
 const INITIAL_STATE = {all:[]}
 
 export default function(state=INITIAL_STATE, action){
   switch(action.type) {
     case FETCH_ALL_GROUPS:
-      console.log('FETCHING ALL GROUPS')
-      console.log("action.payload.data: ", action.payload.data)
       return {all: action.payload.data};
     case FETCH_USER_GROUPS:
       return action.payload.data;
+    case CREATE_NEW_GROUP:
+      return {action.payload.data};
     default:
       return state;
   }
