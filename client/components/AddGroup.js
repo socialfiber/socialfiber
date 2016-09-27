4f4e11b50fdaf5a4087453cd9864a05d4e15e349
Lauren Settle <laurjsett@gmail.com>
2016-09-27 11:32:05 -0700

    [setup] creates AddGroup component scaffolding

---
+++ client/components/AddGroup.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+
+class AddGroup extends Component {
+
+  render(){
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={ handleSubmit() }>
+        <h3>Create a Group Based on Your Dietary Interests</h3>
+          <p>You may choose a topic such as "paleo" or "vegetarian"</p>
+            <div>
+              <label>Name</label>
+                <Field name='topic' component='input' type='text' required/>
+              <label>Description</label>
+                <Field name='description' component='input' type='text' required/>
+            </div>
+              <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
+AddGroup = reduxForm({
+  form: 'AddGroup'
+})(AddGroup)
+
+export default connect()
