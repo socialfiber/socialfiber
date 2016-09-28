af9839286b34da722e9a855fae62f377a2f28c02
Calvin Ton <cton43@yahoo.com>
2016-09-27 20:18:00 -0700

    [feature] adds code to user after survey

--- client/actions/questionnaire.js
+++ client/actions/questionnaire.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 
 export function submitUserStats(userStatsObj) {
-	console.log(userStatsObj)
   userStatsObj.user_id = localStorage.getItem('userID');
   userStatsObj.gender = userStatsObj.gender.value;
+  userStatsObj.preg = userStatsObj.preg.value;
+  userStatsObj.lact = userStatsObj.lact.value;
   userStatsObj.height = userStatsObj.ft*12+userStatsObj.in;
   return axios.post('/api/questions/enterData', userStatsObj)
   	.then((response) => {
