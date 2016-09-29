f2f70ae41216518361d825596962e64520b5deea
Calvin Ton <cton43@yahoo.com>
2016-09-29 15:39:44 -0700

    [refactor] updating reducers

--- client/reducers/auth_reducer.js
+++ client/reducers/auth_reducer.js
@@ -1,16 +1,15 @@
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, PW_DOES_NOT_MATCH } from '../actions/types';
 
-const INITIAL_STATE = {authInfo: {currentUser: ""}, authenticated: null}
+const INITIAL_STATE = { currentUser: undefined, authenticated: false, token: undefined }
 
-export default function(state = [], action) {
+export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
-    case PW_DOES_NOT_MATCH:
-      console.log('PW_DOES_NOT_MATCH')
-      return action.payload;
+    // case PW_DOES_NOT_MATCH:
+    //   return {...state, passwordmatch: action.payload.something };
     case AUTH_USER:
-      return action.payload;
-    case AUTH_ERROR:
-      return action.payload;
+      return {...state, currentUser: action.payload.username, authenticated: true, token: action.payload.token };
+    // case SIGN_OUT:
+    //   return INITIAL_STATE;
     default:
       return state;
   }
