cb228fcd42f4caecab8091b361be866d2a847fe2
Christian Borja <cpborja32@gmail.com>
2016-09-29 17:21:57 -0700

    [refactor] refactors user profile action creators and reducer

--- client/reducers/userProfile_reducer.js
+++ client/reducers/userProfile_reducer.js
@@ -1,12 +1,18 @@
-import { FETCH_USER_DATA, UPDATE_USER_DATA, TOGGLE_EDITING } from '../actions/types';
+import { FETCH_USER_DATA, UPDATE_USER_DATA, FETCH_IDEAL_MACROS, FETCH_ACTUAL_MACROS } from '../actions/types';
 
-export default function(state = null, action) {
+const INITIAL_STATE = { userData: [], idealMacros: [], actualMacros: [] }
+
+export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_USER_DATA:
-      return action.payload;
+      return { ...state, userData: action.payload };
     case UPDATE_USER_DATA:
-      console.log("In userProfile_reducer");
+      // console.log("User information has been updated.");
       return;
+    case FETCH_IDEAL_MACROS:
+      return { ...state, idealMacros: action.payload };
+    case FETCH_ACTUAL_MACROS:
+      return { ...state, actualMacros: action.payload };
     default:
       return state;
   }
