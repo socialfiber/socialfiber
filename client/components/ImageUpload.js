4d4238825a55c6a6ec0b26c14c1588a353985cc1
Lauren Settle <laurjsett@gmail.com>
2016-10-03 11:55:56 -0700

    [pull] adds cloudinary info

--- client/components/ImageUpload.js
+++ client/components/ImageUpload.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 
+const CLOUDINARY_UPLOAD_PRESET = 'l7mlj2bt'
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/tmlthesis/upload'
 
  class ImageUpload extends Component {
 
-  
+  render(){
+    return(
+      <Dropzone multiple={false} accept='image/*' onDrop={this.onImageDrop.bind(this)}>
+        <p>Drag and drop an image or select a file to upload.</p>
+      </Dropzone>
+        )
+  }
 
 
  }
