93f4f601ed39c87f9f7c5979cd9f1d544cc1f7e1
Lauren Settle <laurjsett@gmail.com>
2016-10-06 19:24:03 -0700

    [fix] deletes react bootstrap carousel module

--- client/components/Public/SplashPg.js
+++ client/components/Public/SplashPg.js
@@ -1,43 +1,24 @@
 import React, {Component} from 'react';
+import {React_Boostrap_Carousel} from 'react-boostrap-carousel';
 import {render} from 'react-dom';
 import {Link} from 'react-router';
 
 
 class SplashPg extends Component {
-
-	this.props.callCarousel = {$("#myCarousel").carousel()}
 	render() {
 		return (
-			{this.props.callCarousel}
-			<div id='myCarousel' className = 'carousel slide' data-slide='carousel'>
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
-
-
-				<div class="carousel-inner" role="listbox">
-					<div class="item active">
-						<img src="../assets/orangetree.jpeg" alt="orange"/>
+			<div className='splashpg-div' style={{height:"100%",margin:0}}>
+				<React_Boostrap_Carousel animation={true} className="carousel-fade">
+					<div className="carousel-item-one" style={{backgroundColor:"skyblue"}}>
+						123
 					</div>
-
-					<div class="item">
-						<img src="../assets/lemonsandlimes.jpeg" alt="lemsnlimes"/>
+					<div style={{backgroundColor:"aqua"}}>
+						456
 					</div>
-
-					<div class="item">
-						<img src="../assets/blueberrytree.jpeg" alt="blueberrytree"/>
+					<div style={{backgroundColor:"lightpink"}}>
+						789
 					</div>
-				</div>
-				<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
+				</React_Boostrap_Carousel>
 			</div>
 		);
 	}
