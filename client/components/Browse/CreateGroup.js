72c6d6e28c1cd0688707e2bb293d2029c3a73e41
Lauren Settle <laurjsett@gmail.com>
2016-10-07 11:30:40 -0700

    [pull] adds muithemeprovider to create group component

--- client/components/Browse/CreateGroup.js
+++ client/components/Browse/CreateGroup.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { createNewGroup } from '../../actions/groups';
+import { AutoComplete as MUIAutoComplete } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import { TextField } from 'redux-form-material-ui';
 
 
 class CreateGroup extends Component {
@@ -10,21 +15,27 @@ class CreateGroup extends Component {
 
     const { handleSubmit, submitting } = this.props;
     return (
-      <div>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <div className="container">
+        <div className="row">
+        <div className="col-sm-6 col-md-4 col-md-offset-4">
         <form onSubmit={ handleSubmit(this.props.createNewGroup) }>
           <h4>Create a Group Based on Your Dietary Interests</h4>
           <p>You may choose a topic such as "paleo" or "vegetarian"</p>
           <div>
             <label>Name</label>
-            <Field name='name' component='input' type='text' required/>
+            <Field name='name' component={TextField} type='text' required/>
           </div>
           <div>
             <label>Description</label>
-            <Field name='description' component='input' type='text' required/>
+            <Field name='description' component={TextField} type='text' required/>
           </div>
-          <button type="submit" disabled={submitting} >Submit</button>
+          <button className="creategroupbutton" type="submit" disabled={submitting}>Create Group</button>
         </form>
+        </div>
+        </div>
       </div>
+      </MuiThemeProvider>
     );
     
   }
