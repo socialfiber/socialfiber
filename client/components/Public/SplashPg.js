e662a263753a11623873d1726c7b03d4e71209c0
Lauren Settle <laurjsett@gmail.com>
2016-10-07 09:46:35 -0700

    [pull] reverts to static splash page, adds google font cdn

--- client/components/Public/SplashPg.js
+++ client/components/Public/SplashPg.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {React_Boostrap_Carousel} from 'react-boostrap-carousel';
 import {render} from 'react-dom';
 import {Link} from 'react-router';
 
@@ -7,18 +6,15 @@ import {Link} from 'react-router';
 class SplashPg extends Component {
 	render() {
 		return (
-			<div className='splashpg-div' style={{height:"100%",margin:0}}>
-				<React_Boostrap_Carousel animation={true} className="carousel-fade">
-					<div className="carousel-item-one" style={{backgroundColor:"skyblue"}}>
-						123
-					</div>
-					<div style={{backgroundColor:"aqua"}}>
-						456
-					</div>
-					<div style={{backgroundColor:"lightpink"}}>
-						789
-					</div>
-				</React_Boostrap_Carousel>
+			<div className='splashpg-div'>
+			<nav>
+			    <li className="navItem splashpg"><Link className='signinlink' to='/signin'> Sign In</Link></li>
+			    <li className="navItem splashpg"><Link className='signinlink' to='/signup'> Sign Up</Link></li>
+			</nav>
+			<div className="jumbotron">
+				<h1 className='header-splashpg'>social fiber.</h1>
+				<p className='oneliner'>live healthy, with friends</p>
+			</div>
 			</div>
 		);
 	}
