0b70d09a7aab6b917aaf0ef50075688c48970a4b
Sam Kim <samkim28@gmail.com>
2016-10-07 16:17:20 -0700

    [style] adds material form to reply comment box

--- client/components/Groups/CommentBox.js
+++ client/components/Groups/CommentBox.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { postComment } from '../../actions/groups';
-
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { TextField } from 'redux-form-material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class CommentBox extends Component {
 
@@ -11,15 +14,16 @@ class CommentBox extends Component {
     const { handleSubmit, submitting } = this.props;
 
     return (
-      <div>
-        <form onSubmit = {handleSubmit(this.props.postComment)}>
-            <div>
-              <label>Reply</label>
-              <Field name="message" component="input" type="text" required />
-            </div>
-            <button type="submit" disabled={submitting} >Submit</button>
-        </form>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <form onSubmit = {handleSubmit(this.props.postComment)}>
+              <div>
+                <Field name="message" component={TextField} multiLine={true} floatingLabelText="Reply" type="text" required />
+              </div>
+              <RaisedButton type="submit" disabled={submitting} label="Submit" />
+          </form>
+        </div>
+      </MuiThemeProvider>
     );
 
   }
