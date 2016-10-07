aacdcb104313e7c1cbb50297e40de9e1507c4991
Lauren Settle <laurjsett@gmail.com>
2016-10-07 14:27:22 -0700

    [style] styles create a group component

--- client/components/Browse/CreateGroup.js
+++ client/components/Browse/CreateGroup.js
@@ -16,30 +16,30 @@ class CreateGroup extends Component {
     const { handleSubmit, submitting } = this.props;
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <div className="container">
-        <div className="row">
-        <div className="col-sm-6 col-md-4 col-md-offset-4">
-        <form onSubmit={ handleSubmit(this.props.createNewGroup) }>
-          <h4>Create a Group Based on Your Dietary Interests</h4>
-          <p>You may choose a topic such as "paleo" or "vegetarian"</p>
-          <div>
-            <label>Name</label>
-            <Field name='name' component={TextField} type='text' required/>
-          </div>
-          <div>
-            <label>Description</label>
-            <Field name='description' component={TextField} type='text' required/>
-          </div>
-          <button className="creategroupbutton" type="submit" disabled={submitting}>Create Group</button>
-        </form>
+        <div className="container create group">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-md-offset-4">
+              <form onSubmit={ handleSubmit(this.props.createNewGroup) }>
+                <h4>Create a Group Based on Your Dietary Interests</h4>
+                <p className="choose-topic">You may choose a topic such as "paleo" or "vegetarian"</p>
+                <div>
+                  <label>Name</label>
+                  <Field name='name' component={TextField} type='text' required/>
+                </div>
+                <div>
+                  <label>Description</label>
+                  <Field name='description' component={TextField} type='text' required/>
+                </div>
+                <button className="creategroupbutton btn btn-secondary" type="submit" disabled={submitting}>Create Group</button>
+              </form>
         </div>
         </div>
       </div>
       </MuiThemeProvider>
     );
-    
+
   }
-  
+
 }
 
 CreateGroup = reduxForm({
