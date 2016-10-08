521a19c678325db3a5cb0142b8e75d3897aa5851
Lauren Settle <laurjsett@gmail.com>
2016-10-08 15:32:10 -0700

    [style] styles table for food diary entry log

--- client/components/User/Tabs/FoodDiaryLog.js
+++ client/components/User/Tabs/FoodDiaryLog.js
@@ -4,15 +4,15 @@ import { deleteFoodDiaryEntry } from '../../../actions/foodDiary';
 const FoodDiaryLog = (props) => {
 	return (
 			<tr className="foodDiaryLogtr">
-				<td>{props.log.qty}</td>
-				<td>{props.log.food}</td>
-				<td>{props.log.storage.cal}</td>
-				<td>{props.log.storage.carb}</td>
-				<td>{props.log.storage.prot}</td>
-				<td>{props.log.storage.fat}</td>
-				<td>{props.log.storage.fib}</td>
-				<td>{props.log.storage.n6}</td>
-				<td onClick={()=>{deleteFoodDiaryEntry(props.log)}}>x</td>
+				<td className="fdltd">{props.log.qty}</td>
+				<td className="fdltd">{props.log.food}</td>
+				<td className="fdltd">{props.log.storage.cal}</td>
+				<td className="fdltd">{props.log.storage.carb}</td>
+				<td className="fdltd">{props.log.storage.prot}</td>
+				<td className="fdltd">{props.log.storage.fat}</td>
+				<td className="fdltd">{props.log.storage.fib}</td>
+				<td className="fdltd">{props.log.storage.n6}</td>
+				<td className="fdltd" onClick={()=>{deleteFoodDiaryEntry(props.log)}}>x</td>
 				</tr>
 	);
 }
