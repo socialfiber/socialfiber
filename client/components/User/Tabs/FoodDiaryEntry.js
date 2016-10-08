47b366ad7099839d0419b67b511f74836aca9147
Lauren Settle <laurjsett@gmail.com>
2016-10-08 14:24:37 -0700

    [style] adds mui theme to food diary entry

--- client/components/User/Tabs/FoodDiaryEntry.js
+++ client/components/User/Tabs/FoodDiaryEntry.js
@@ -2,6 +2,11 @@
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { submitFoodDiaryEntry, fetchFoodDiary, fetchMacros } from '../../../actions/foodDiary';
+import { AutoComplete as MUIAutoComplete } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import { TextField } from 'redux-form-material-ui';
 
 
 class FoodDiaryEntry extends Component {
@@ -27,20 +32,22 @@ class FoodDiaryEntry extends Component {
     const today = new Date().toISOString().substr(0,10);
 
     return (
-      <form onSubmit={ handleSubmit(this.submitEntry) } >
-        <h3>Add Entry</h3>
-        <p>Please submit a meal.</p>
-        <div>
-          <label>Date</label>
-          <Field name="date" component="input" type="date" max={today} required />
-          <label>qty/srv</label>
-          <Field name="qty" component="input" type="number" min="1" required />
-          <label>Tell us what you ate</label>
-          <Field name="food" component="input" type="text" required />
-        </div>
-        <button type="submit" disabled={submitting} >Submit</button>
-      </form>
-    );
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <form onSubmit={ handleSubmit(this.submitEntry) } >
+          <h3>Add Entry</h3>
+          <p>Please submit a meal.</p>
+          <div>
+            <label>Date</label>
+            <Field name="date" component="input" type="date" max={today} required />
+            <label>qty/srv</label>
+            <Field name="qty" component="input" type="number" min="1" required />
+            <label>Tell us what you ate</label>
+            <Field name="food" component="input" type="text" required />
+          </div>
+          <button type="submit" disabled={submitting} >Submit</button>
+        </form>
+      </MuiThemeProvider>
+        );
 
   }
 
