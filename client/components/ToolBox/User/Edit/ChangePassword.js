62293e3cdfb84cf7a081c4491ec5ae9807c009ec
Lauren Settle <laurjsett@gmail.com>
2016-10-09 12:03:22 -0700

    [style] edits return to user pf button

---
+++ client/components/ToolBox/User/Edit/ChangePassword.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { submitChangePassword, resetError } from '../../../actions/users';
+import { AutoComplete as MUIAutoComplete } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import { TextField } from 'redux-form-material-ui';
+
+
+class ChangePassword extends Component {
+
+  componentWillUnmount() {
+    this.props.resetError();
+  }
+
+  render() {
+
+    const { handleSubmit, submitting } = this.props;
+
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <form onSubmit={handleSubmit(this.props.submitChangePassword)} >
+            <div>
+              <label>Password</label>
+              <Field name="password" component={TextField} type="password" />
+            </div>
+            <div>
+              <label>Confirm Password</label>
+              <Field name="confirmPW" component={TextField} type="password" />
+            </div>
+            <div>
+              <label>New Password</label>
+              <Field name="newPW" component={TextField} type="password" />
+            </div>
+            <button className="changePW-btn btn btn-secondary" type="submit" disabled={submitting} >Submit</button>
+          </form>
+        </div>
+      </MuiThemeProvider>
+    );
+
+  }
+
+}
+
+ChangePassword = reduxForm({
+  form: 'ChangePasswordForm'
+})(ChangePassword);
+
+const mapStateToProps = (state) => {
+  return {
+    msg: state.userProfile.changePW
+  }
+}
+
+export default connect(mapStateToProps, { submitChangePassword, resetError })(ChangePassword);
---
+++ client/components/ToolBox/User/Edit/UpdateQuestionnaire.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { updateUserData } from '../../../actions/users';
+import Cookies from 'js-cookie';
+import SelectComponent from '../../ToolBox/SelectComponent';
+import { AutoComplete as MUIAutoComplete } from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+import { TextField } from 'redux-form-material-ui';
+
+
+class UpdateQuestionnaire extends Component {
+
+  componentWillUnmount() {
+    Cookies.remove('userGender');
+  }
+
+  render() {
+
+    Cookies.set('userGender', this.props.userData.gender);
+    const { handleSubmit, submitting } = this.props;
+    const pregnantOptions = [{value: true, label: 'true'}, {value: false, label: 'false'}];
+    const lactatingOptions = [{value: true, label: 'true'}, {value: false, label: 'false'}];
+    const femaleQuestions = () => {
+      if(this.props.userData.gender === 'female') {
+        return (
+          <div>
+            <div>
+              <label>Are you currently pregnant?</label>
+              <Field name="preg" component={SelectComponent} options={pregnantOptions} placeholder={this.props.userData.preg} />
+            </div>
+            <div>
+              <label>Are you currently lactating?</label>
+              <Field name="lact" component={SelectComponent} options={lactatingOptions} placeholder={this.props.userData.lact} />
+            </div>
+          </div>
+        );
+      }
+    }
+
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <form onSubmit={handleSubmit(this.props.updateUserData)}>
+            <div>
+              <label>Age</label>
+              <Field name="age" component={TextField} type="number" min="13" placeholder={this.props.userData.age} required />
+            </div>
+            <div>
+              <label>Height (ft & in)</label>
+              <Field name="ft" component="input" type="number" min ="4" max="8" placeholder={Math.floor(this.props.userData.height / 12)} required />
+              <Field name="in" component="input" type="number" min="0" max="11" placeholder={this.props.userData.height % 12} />
+            </div>
+            <div>
+              <label>Weight</label>
+              <Field name="weight" component={TextField} type="number" min="70" placeholder={this.props.userData.weight} required />
+            </div>
+            {femaleQuestions()}
+            <button className="updateQ-btn btn btn-secondary" type="submit" disabled={submitting} >Submit</button>
+          </form>
+        </div>
+      </MuiThemeProvider>
+    );
+
+  }
+
+}
+
+UpdateQuestionnaire = reduxForm({
+  form: 'UpdateQuestionnaireForm'
+})(UpdateQuestionnaire);
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.userProfile.userData,
+  }
+}
+
+export default connect(mapStateToProps, { updateUserData })(UpdateQuestionnaire);
